c0d15664c57fe5ba44928cdd48bda5e01ca1b719
justinlamarr1992 <58798470+justinlamarr1992@users.noreply.github.com>
2021-05-08 22:38:03 -0500

    First Commit of React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="body">
+      <header>
+    </header>
+    <main>
+      <section className="left">
+        <h1 className="learn">
+          Learn to code by <br />
+          watching others
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          See how experienced developers solve problems in real-time. Watching
+          scripted tutorials is great, but understanding how developers think is
+          invaluable.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </section>
+      <section className="right">
+        <div className="top">
+          <p className="trial">
+            <span className="bold">Try it free 7 days</span> then $20/mo. thereafter
+          </p>
+        </div>
+        <div className="bottom">
+          <form className="info" action="index.html" method="post">
+            <input type="text" name="" placeholder="First Name" />
+            <input type="text" name="" placeholder="Last Name" />
+            <input type="email" name="" placeholder="Email Address" />
+            <input type="password" name="" placeholder="Password" />
+
+            <button type="button" name="button">CLAIM YOUR FREE TRAIL</button>
+          </form>
+
+          <p className="terms">
+            By clicking the button, you are agreeing to our 
+            <a href="#"> Terms and Services</a>
+          </p>
+        </div>
+      </section>
+    </main>
+    <footer></footer></div>
+    
   );
 }
 
